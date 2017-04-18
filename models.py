487c484d4fe7b5f0b706f7322c2b7b682e02f371
sapd <sapd@vccloud.vn>
2017-04-19 00:16:16 +0700

    MOD: su dung pid

--- models.py
+++ models.py
@@ -5,13 +5,13 @@ class Posts(Model):
     """
     Model for Posts table
     """
-    post_id = Field()
+    pid = Field()
     post_title = Field()
     post_content = Text()
     table_role = Field()
 
     def __repr__(self):
-        return str({"post_id": self.post_id,
+        return str({"pid": self.pid,
                     "post_title": self.post_title,
                     "post_content": self.post_content,
-                    "table_type": self.table_role})
+                    "table_role": self.table_role})
